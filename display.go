ef0fa3f801b372c069aa2fd55fdb3ceeb8d9e775
Tio Gobin <tiogobin@gmail.com>
2024-12-04 14:26:50 +0100

    [rework] graph type splited in defferent functions

--- display.go
+++ display.go
@@ -5,8 +5,6 @@ import (
 )
 
 
-
-
 type Coord struct {
 	x int
 	y int
--- graph.go
+++ graph.go
@@ -23,45 +23,56 @@ type Graph struct {
 }
 
 
+func sorted_bargraph(rows []string, sep string, g Graph, color_names []string) (Graph) {
+	var row_nbr int = 0
+	var col_nbr int = 1
+	for row_nbr < len(rows) {
+		var color = color_names[col_nbr]
+		var row []string = strings.Split(rows[row_nbr], sep)
+		val, _ := strconv.Atoi(row[col_nbr])
+		var bar = Bar{x: row_nbr, y: val, color:color}
+		g.bars = append(g.bars, bar)
+		g.nbr_col = len(row)
+		row_nbr++
+	if row_nbr == len(rows) && col_nbr != len(row) - 1 {
+			row_nbr = 0
+			col_nbr++
+		}
+	}
+	return g
+}
+
+
+func bar_graph(rows []string, sep string, g Graph, color_names []string) (Graph) {
+	for i, _ := range(rows) {
+		var row []string = strings.Split(rows[i], sep)
+		row = row[1:]
+		for j, val := range(row) {
+			g.nbr_col = len(row)
+			var color = color_names[j%g.nbr_col]
+			var y, err = strconv.Atoi(val)
+			if err != nil {
+				fmt.Println("converison error:", err)
+				os.Exit(1)
+			}
+			var b = Bar{x: i, y: y, color: color}
+			g.bars = append(g.bars, b)
+		}	
+	}
+	return g
+}
+
 // TODO 2 functions for clarity for sorted
 func create_graph(file string, sep string, sorted bool) (Graph) {
 	var rows []string = strings.Split(file, "\n")
 	rows = rows[:len(rows)-1]
 	var g = Graph{}
-
 	color_names := []string{"maroon","green","olive","navy","purple","teal","silver","gray","red","lime","yellow","blue"}
+	// Switch on type
 	if sorted {
-		var row_nbr int = 0
-		var col_nbr int = 1
-		for row_nbr < len(rows) {
-			var color = color_names[col_nbr]
-			var row []string = strings.Split(rows[row_nbr], sep)
-			val, _ := strconv.Atoi(row[col_nbr])
-			var bar = Bar{x: row_nbr, y: val, color:color}
-			g.bars = append(g.bars, bar)
-			g.nbr_col = len(row)
-			row_nbr++
-		if row_nbr == len(rows) && col_nbr != len(row) - 1 {
-				row_nbr = 0
-				col_nbr++
-			}
-		}
+		g = sorted_bargraph(rows, sep, g, color_names)
 	} else {
-		for i, _ := range(rows) {
-			var row []string = strings.Split(rows[i], sep)
-			row = row[1:]
-			for j, val := range(row) {
-				g.nbr_col = len(row)
-				var color = color_names[j%g.nbr_col]
-				var y, err = strconv.Atoi(val)
-				if err != nil {
-					fmt.Println("converison error:", err)
-					os.Exit(1)
-				}
-				var b = Bar{x: i, y: y, color: color}
-				g.bars = append(g.bars, b)
-			}	
-		}
+		g = bar_graph(rows, sep, g, color_names)
 	}
 	return g
 }
